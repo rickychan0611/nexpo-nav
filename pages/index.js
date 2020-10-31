7b48f50b8bbe8eb4f6e38399cedf2cb9541fdcf0
rickychan0611 <ric0611@gmail.com>
2020-10-31 16:50:04 -0700

    added styled-component

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import React from "react";
 import { Button } from "react-native-elements";
 import { View, Text, StyleSheet } from "react-native";
 import { Link, useRouting } from "expo-next-react-navigation";
+import styled from "styled-components/native";
 
 export default function Home() {
   const { navigate } = useRouting();
 
   return (
     <View style={styles.container}>
+      <Title>Title</Title>
       <Text style={styles.text}>Home Screen 🥳</Text>
       <Button
         title="Click me to open profile :)"
@@ -22,6 +24,10 @@ export default function Home() {
     </View>
   );
 }
+
+const Title = styled.Text`
+  color: red;
+`;
 
 const styles = StyleSheet.create({
     container: {
