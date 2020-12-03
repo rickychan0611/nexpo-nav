7bfd010f5483f9677a42850ad1cf5bb9b68012bb
rickychan0611 <ric0611@gmail.com>
2020-12-02 20:11:03 -0800

    mobile print invoice

--- components/Printer/invoiceHTML.js
+++ components/Printer/invoiceHTML.js
@@ -6,81 +6,88 @@ import moment from 'moment';
 const invoiceHTML = (order) => {
     return `
     <!DOCTYPE html>
-<html>
-<body>
-<style>
+    <html>
+    <body>
+    <style>
 
-@media all {
-  .page-break {
-    display: none;
-  }
-}
- 
-@media print {
-  html, body {
-    height: initial !important;
-    overflow: initial !important;
-    -webkit-print-color-adjust: exact;
+    @media all {
+    .page-break {
+        display: none;
+    }
+    }
+    
+    @media print {
+    html, body {
+        height: initial !important;
+        overflow: initial !important;
+        -webkit-print-color-adjust: exact;
+        font-size: 16px;
+    }
+    }
+    
+    @media print {
+    .page-break {
+        margin-top: 1rem;
+        display: block;
+        page-break-before: auto;
+    }
+    }
+    
+    @page {
+    size: auto;
+    margin: 20mm;
+    }
+
+    .table {
+        width: 100%;
+        text-align: center;
+        border: 1px solid black;
+        font-size: 16px;
+    }
+
+    .row {
+        /* padding: 15px; */
+        width: 100%;
+        border-bottom: 1px solid black;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+    }
+
+    .cell {
+    margin: 10px;
     font-size: 16px;
-  }
-}
- 
-@media print {
-  .page-break {
-    margin-top: 1rem;
-    display: block;
-    page-break-before: auto;
-  }
-}
- 
-@page {
-  size: auto;
-  margin: 20mm;
-}
+    }
 
-.table {
-    width: 100%;
-    text-align: center;
-    border: 1px solid black;
+    .pad {
+    padding: 7px;
     font-size: 16px;
-}
+    }
 
-.row {
-    /* padding: 15px; */
-    width: 100%;
+    .bottomLine {
     border-bottom: 1px solid black;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: nowrap;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-}
+    font-size: 16px;
+    }
 
-.cell {
-  margin: 10px;
-  font-size: 16px;
-}
+    .rightLine {
+    border-right: 1px solid black;
+    font-size: 16px;
+    }
 
-.pad {
-  padding: 10px;
-  font-size: 1px;
-}
-
-.bottomLine {
-  border-bottom: 1px solid black;
-}
-
-.rightLine {
-  border-right: 1px solid black;
-}
-
-.left {
-    text-align: left;
-}
-.center {
-    text-align: center;
-}
+    .left {
+        text-align: left;
+        font-size: 16px;
+    }
+    .center {
+        text-align: center;
+        font-size: 16px;
+    }
+    .grey {
+        background-color: rgb(238, 238, 238);
+    }
 </style>
     <div class="table">
           <div class="row" style="border-bottom: 0; margin: 15px;">
@@ -151,46 +158,46 @@ const invoiceHTML = (order) => {
 
           </div>
 
-          <table style="width: 100%; border-collapse: collapse;">
+          <table style="width: 100%; border-collapse: collapse; margin-top: -18px;">
             <tbody>
-              <tr class="bottomLine grey" style="border: 1px solid black;">
-                <td class="rightLine pad" style="width: 60%;">名稱</td>
+              <tr class="bottomLine grey pad">
+                <td class="rightLine pad" style="width: 60%, font-size: 16px;">名稱</td>
                 <td class="rightLine center pad">數量</td>
                 <td class="rightLine center pad">單價</td>
-                <td class="center">總額</td>
+                <td class="center pad">總額</td>
               </tr>
               ${order.orderItems.map((orderItem) => {
-        let item = orderItem.item
-        return (
-            <tr class="bottomLine">
-                <td class="rightLine pad" style="width:60%;">{item.chineseName} ({item.unit})</td>
-                <td class="rightLine center pad">{orderItem.quantity}</td>
-                <td class="rightLine center pad">{item.final_price}</td>
-                <td class="center pad">${(+item.final_price * +orderItem.quantity).toFixed(2)}</td>
-            </tr>
-        )
-    })}
+                let item = orderItem.item
+                return (
+                    `<tr class="bottomLine pad">
+                        <td class="rightLine pad left" style="width:60%;">${item.chineseName} (${item.unit})</td>
+                        <td class="rightLine center pad">${orderItem.quantity}</td>
+                        <td class="rightLine center pad">${item.final_price}</td>
+                        <td class="center pad">${"$" + (+item.final_price * +orderItem.quantity).toFixed(2)}</td>
+                    </tr>`
+                )
+            })}
               <tr class="bottomLine left">
                 <td colspan="4" class="pad">備注：</td>
               </tr>
               <tr class="bottomLine left">
                 <td colspan="4" class="pad">配送信息：${order.shippingAddress.message}<br />
-            用戶名：${order.shippingAddress.firstName} ${order.shippingAddress.lastName}<br />
-            聯系電話：${order.shippingAddress.phoneNumber}<br />
-            運費：${order.shippingFee}<br />
-            地址：<br/>
-            ${order.shippingAddress.address1} <br/>
-            ${order.shippingAddress.address2 && <> ${order.shippingAddress.address2} <br /> </>}
-            ${order.shippingAddress.city}, ${order.shippingAddress.province} <br/>
-            ${order.shippingAddress.postalCode}
-            </td>
+                用戶名：${order.shippingAddress.firstName} ${order.shippingAddress.lastName}<br />
+                聯系電話：${order.shippingAddress.phoneNumber}<br />
+                運費：${order.shippingFee}<br />
+                地址：<br/>
+                ${order.shippingAddress.address1} <br/>
+                ${order.shippingAddress.address2 && <> ${order.shippingAddress.address2} <br /> </>}
+                ${order.shippingAddress.city}, ${order.shippingAddress.province} <br/>
+                ${order.shippingAddress.postalCode}
+                </td>
               </tr>
             </tbody>
           </table>
         </div>
         </body>
-</html>
-        `
-}
+    </html>
+     `
+    }
 
 export default invoiceHTML;
